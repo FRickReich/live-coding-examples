afe7d382b1a2fb36918b54d8fbe8a16e7a076fcf
FRickReich <frickreich@gmail.com>
2021-01-26 09:00:59 +0100

    Add conditional statements

---
+++ Programming-Basics/2_program/0_conditional-statements.js
@@ -0,0 +1,38 @@
+// https://www.javascript.com/learn/conditionals
+
+const testValue = 5;
+
+// If-Statement:
+    // if the condition is true, this block will be executed
+    if(testValue === 5)
+    {
+        'testValue ist 5';
+    }
+
+// Else-Statement:
+    //  block of code inside of "else" statement will be executed if the condition is false
+    const today = "tuesday";
+
+    if (today === "monday")
+    {
+        'today is monday';
+    }
+    else
+    {
+        'today is not monday';
+    }
+
+// Else-If-Statement:
+    // block of code inside of "else if" block will be executed if the condition is matched.
+    if (today === "monday")
+    {
+        console.log('today is monday');
+    }
+    else if (today === "tuesday")
+    {
+        console.log('today is tuesday');
+    }
+    else
+    {
+        console.log('today is some other day');
+    }
---
+++ Programming-Basics/2_program/1_switches.js
@@ -0,0 +1,73 @@
+// https://love2dev.com/blog/javascript-switch-statement/
+
+const testValue = 3;
+
+// Beispiel mit 'default' Wert:
+switch(testValue) {
+    case 1:
+        console.log("testValue ist 1");
+        break;
+    case 2:
+        console.log("testValue ist 2");
+        break;
+    default:
+      console.log("testValue hat einen anderen wert");
+}
+
+// Switches vergleichen strict, mit ===
+const testValue2 = "0"; // funktioniert: const testValue2 = 0;
+
+switch (testValue2) {
+  case 0:
+    console.log("Off");
+    break;
+  case 1:
+    console.log("On");
+    break;
+  default:
+    console.log("No value found");
+}
+
+
+// Mehrere switch statements - Wochenbeispiel:
+const today = "Sunday"
+
+switch (today) {
+    case "Monday":
+        console.log("Today is Sunday");
+        break;
+    case "Tuesday":
+        console.log("Today is Sunday");
+        break;
+    case "Wednesday":
+        console.log("Today is Sunday");
+        break;
+    case "Thursday":
+        console.log("Today is Sunday");
+        break;
+    case "Friday":
+        console.log("Today is Sunday");
+        break;
+    case "Saturday":
+        console.log("Today is Sunday");
+        break;
+    case "Sunday":
+        console.log("Today is Sunday");
+}
+
+// Mehrere cases für das selbe ergebnis, wochenend/arbeitswochen-Beispiel:
+const dayOfWeek = "Monday";
+
+switch (dayOfWeek.toLowerCase()) {
+    case "monday":
+    case "tuesday":
+    case "wednesday":
+    case "thursday":
+    case "friday":
+    default:
+        console.log("Arbeiten...");
+        break;
+    case "saturday":
+    case "sunday":
+        console.log("Wochenende!");
+}
